e6cd2ea0fe033c29bdbe8296e05ff4717ea3f19b
Zakiy01 <joraxonovziyovuddin@gmail.com>
2022-02-03 01:39:00 +0500

    Loader

---
+++ script.js
@@ -0,0 +1,19 @@
+// setTimeout(function () {
+//     alert('hi')
+// }), 5000
+
+$(window).on("load", function () {
+    $(".box1").fadeOut("slow")
+})
+
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const loader = document.querySelector('.loader')
+
+//     setTimeout(() => {
+//         loader.style.opacity = '0'
+//         setTimeout(() => {
+//             loader.style.display = 'none'
+//         }), 500
+//     }), 2000
+// })
